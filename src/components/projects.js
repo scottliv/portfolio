de9ee4e9d8363a890be286a38ee7c527918b2c1d
scottlivingstone-forge <scott@forgeandsmith.com>
2019-07-31 21:06:52 -0700

    Add different project

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,31 +6,27 @@ const projects = [
   {
     title: 'Shark Club',
     link: 'https://www.sharkclub.com',
-    desc:
-      'A web app that lets users search for all kinds of recipes from around the world. Fetches data using the Edaman API. Built with React, Redux, and Bootstrap',
+    desc: '',
     image: 'sharkclub.png'
   },
   {
     title: 'Pearl Of Burgundy',
     link: 'https://www.pearlofburgundy.com/',
-    desc:
-      'A brochure site aimed to promote tourism in Dumaguete City Philippines. Built with React, Sass and Bootstrap.',
+    desc: '',
     image: 'pob.png'
   },
   {
     title: 'Inclusion BC',
     link: 'https://inclusionbc.org/',
-    desc:
-      'An e-commerce app that lets users filter through items in real-time using multiple filters (e.g. type, color, size, price). Built with React, Redux, and Sass',
+    desc: '',
 
     image: 'inclusion.png'
   },
   {
-    title: 'Sue Hollis',
-    link: 'https://www.suehollis.com/',
-    desc:
-      'A web app that lets customers customize their burgers by adding the ingredients they want. Built with React',
-    image: 'suehollis.png'
+    title: 'Storkcraft',
+    link: 'https://storkcraftdirect.com/',
+    desc: '',
+    image: 'storkcraft.png'
   },
 ];
 
